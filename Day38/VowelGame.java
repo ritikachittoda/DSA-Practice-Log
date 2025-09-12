e721a0ad4480fb51097378f89b6155c1fe228850
Ritika Chittoda <ritikachittoda@gmail.com>
2025-09-12 23:37:51 +0530

    Day 38 - Vowels Game in a String and Linked List Insertion at End

--- Day38/VowelGame.java
+++ Day38/VowelGame.java
@@ -5,13 +5,16 @@ package Day38;
 
 public class VowelGame {
     public boolean doesAliceWin(String s) {
+
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
+            
             switch(c) {
                 case 'a','e','i','o','u':
                     return true;
             }
         }
+
         return false;
     }
 }
